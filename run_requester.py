5b5855a5ac6c7f1de57b632b5db6b9471afbb092
fluffyhake <haakon.reppen@gmail.com>
2020-11-26 19:41:46 +0100

    It now runs!

--- run_requester.py
+++ run_requester.py
@@ -1,6 +1,11 @@
-
+import os
 from netmiko import Netmiko
 from getpass import getpass
+from datetime import datetime
+import time
+
+currenttime = datetime.now()
+currenttimerightformat = currenttime.strftime("%d/%m/%Y %H:%M:%S")
 
 username = "haakonlab"
 password = "lablab" #(or getpass)
@@ -46,12 +51,21 @@ my_devices = {
         "device_type" : "cisco_ios"
     }
 }   
+#resets the running-config-all.txt file
+configtxt = open("running-config-all.txt", "w")
+configtxt.write("-" * 30 + currenttimerightformat + "-" * 30 + "\n" * 10)
+
+
+
+
+
 
 
 for host in my_devices:
     print(host + ":")
     print(my_devices[host])
     print("-" *30 + "DEBUG" + "-" *30 )
+
 
 
 for host in my_devices:
@@ -62,9 +76,35 @@ for host in my_devices:
         output = net_conn.send_command("show run")
         print(output)
         print("-" * 60)
+        try:
+            print("Attempting to write to running-config-all.txt")
+            configtxt.write("\n" *2 + "-" *30 + host + ":" +"-" *30)
+            configtxt.write("\n" + output + "\n \n \n")
+
+        except:
+            print("Writing" + host + "to file went wrong...")
 
     except:
         print("Device does not support SSH :(")
         print("My lab is too old for this... i need to add telnet support")
         print("-" * 60)
+
+configtxt.write("-" * 30 + "END" + "-" *30)
+
+
+time.sleep(5)
+
+#Removing secrets and sensitive info section:
+censor_words = ["secret", "password", "username", "ip domain-name"]
+
+
+
+with open('running-config-all.txt') as oldfile, open('running-config-all-censored.txt', 'w') as newfile:
+    for line in oldfile:
+        if not any(censor_word in line for censor_word in censor_words):
+            newfile.write(line)
+
+newfile.close()
+
+
 
